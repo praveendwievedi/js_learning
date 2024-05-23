c6d71a9a54e75759e1eb2511f4b20de23fddb64d
Praveen Dwivedi <121290329+praveendwievedi@users.noreply.github.com>
2024-05-23 16:32:17 +0000

    datatypes in JS

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // this line means we gonna use moderen version of code 
+
+let name=""
+let age= 18;
+let dob=null;
+
+
+/*
+data types in JavaScript:
+number => 2 to power 53;
+bigInt
+string 
+boolnean=> true/false.
+symbol
+object
+null
+undefined.
+*/
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof dob);
+
+console.log(typeof null);// type is object... 
+console.log(typeof undefined);// type is  undefined...
