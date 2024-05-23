cc0c909d77a9c5653a682858dcfdacd38b382f6c
Praveen Dwivedi <121290329+praveendwievedi@users.noreply.github.com>
2024-05-23 16:32:46 +0000

    Conversion Operation in JS

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,21 @@
+let  score="33abs"
+
+let scoreConsert=Number(score);
+// console.log(scoreConsert);
+/*
+33=> 33;
+"33abs"= nan(not a number).
+null=0
+undefined= nan
+true=>1/false= 0. 
+*/
+let isCorrect=33;
+
+let new_Is_Correct=Boolean(isCorrect);
+console.log(new_Is_Correct);
+
+/*
+number==0=> false/number>0=>true.
+""= false.
+"ans"=true.
+*/
